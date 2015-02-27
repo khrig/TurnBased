fb2e191c63fcde167be2d355be32e217707c1e04
Henrik Aronsson <arnssn@gmail.com>
2015-02-27 10:22:16 +0100

    Forgot return statement for grid generation
    Shit happends when coding in the online editor ;)

--- TurnBased/WorldController.cs
+++ TurnBased/WorldController.cs
@@ -65,12 +65,13 @@ namespace TurnBased {
         
         private string[,] GetBackground() {
             int height = 16, width = 20;
-		string[,] grid = new string[height,width];
-		for(int i = 0; i < height; i++) {
-			for(int j = 0; j < width; j++) {
-				grid[i,j] = "bkg";
-			}
+	    string[,] grid = new string[height,width];
+	    for(int i = 0; i < height; i++) {
+		for(int j = 0; j < width; j++) {
+		     grid[i,j] = "bkg";
 		}
+	    }
+	    return grid;
         }
     }
 }
